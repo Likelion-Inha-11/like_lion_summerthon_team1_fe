b4fc8abacee980a62d5a6e8218580067ca2d941d
sunjae <zxsunje775@naver.com>
2023-07-11 19:12:22 +0900

    2)

--- src/page/ChatPage.jsx
+++ src/page/ChatPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 const ChatPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleChat = () => {
+    axios
+      .get("/chat/")
+      .then((response) => {
+        // 채팅 성공 처리
+      })
+      .catch((error) => {
+        // 오류 처리
+      });
+  };
+
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <button onClick={handleChat}>Start Chat</button>
+    </div>
+  );
 };
 
 export default ChatPage;
