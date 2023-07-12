bf196d960ca7b25888bf5a74a7b6d588e9a1288d
sunjae <zxsunje775@naver.com>
2023-07-12 14:24:48 +0900

    0711 쟝고채팅구현하려고

--- src/page/ChatPage.jsx
+++ src/page/ChatPage.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-const ChatPage = (htmlTemplate) => {
+const ChatPage = () => {
+  const [htmlTemplate, setHtmlTemplate] = useState(""); // 상태 변수로 선언
+
   const handleChat = () => {
     axios
       .get("/chat/")
       .then((response) => {
         // 채팅 성공 처리
         console.log("성공!!");
-        const htmlTemplate = response.data;
+        setHtmlTemplate(response.data); // 상태 업데이트
       })
       .catch((error) => {
         console.log("에러!!");
@@ -18,8 +21,11 @@ const ChatPage = (htmlTemplate) => {
 
   return (
     <>
-      <div dangerouslySetInnerHTML={{ __html: htmlTemplate }} />;
+      <div dangerouslySetInnerHTML={{ __html: htmlTemplate }} />
       <button onClick={handleChat}>Start Chat</button>
+      {/* 원하는 방식으로 화면을 구성 */}
+      {htmlTemplate}
+      <Link to="http://127.0.0.1:8000/chat/">채팅</Link>
     </>
   );
 };
