15015c98e67b3c496c402aa50df09f56c179ad96
sunjae <zxsunje775@naver.com>
2023-07-12 17:22:22 +0900

    임시

--- src/page/ChatPage.jsx
+++ src/page/ChatPage.jsx
@@ -1,31 +1,53 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ChatPage = () => {
-  const [htmlTemplate, setHtmlTemplate] = useState(""); // 상태 변수로 선언
+  const [message, setMessage] = useState("");
+  const [chatLog, setChatLog] = useState([]);
+  const [ws, setWs] = useState(null);
 
-  const handleChat = () => {
-    axios
-      .get("/chat/")
-      .then((response) => {
-        // 채팅 성공 처리
-        console.log("성공!!");
-        setHtmlTemplate(response.data); // 상태 업데이트
-      })
-      .catch((error) => {
-        console.log("에러!!");
-        // 오류 처리
-      });
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000/chat/");
+    ws.onopen = () => {
+      console.log("connected to server");
+    };
+
+    ws.onmessage = (event) => {
+      setChatLog((chatLog) => [...chatLog, event.data]);
+    };
+
+    ws.onclose = () => {
+      console.log("disconnected from server");
+    };
+
+    setWs(ws);
+    return () => ws.close();
+  }, []);
+
+  const handleChat = (event) => {
+    event.preventDefault();
+    if (ws) {
+      ws.send(message);
+      setMessage("");
+    }
   };
 
   return (
     <>
-      <div dangerouslySetInnerHTML={{ __html: htmlTemplate }} />
-      <button onClick={handleChat}>Start Chat</button>
-      {/* 원하는 방식으로 화면을 구성 */}
-      {htmlTemplate}
-      <Link to="http://127.0.0.1:8000/chat/">채팅</Link>
+      <ul>
+        {chatLog.map((msg, idx) => (
+          <li key={idx}>{msg}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleChat}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send Message</button>
+      </form>
+      <Link to="http://127.0.0.1:8000/chat/">Go Back</Link>
     </>
   );
 };
