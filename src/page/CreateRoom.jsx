164111ea9202cdf9a5ff003bfba4b84e230e1a62
nahyeon <k92544199@gmail.com>
2023-07-14 21:16:14 +0900

    배포제발하자

--- src/page/CreateRoom.jsx
+++ src/page/CreateRoom.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { styled, css } from "styled-components";
+import { styled } from "styled-components";
 import { useState } from "react";
 import BottomBar from "./component/SearchPageCom/bottomBar";
 import { useNavigate, useParams } from "react-router-dom";
