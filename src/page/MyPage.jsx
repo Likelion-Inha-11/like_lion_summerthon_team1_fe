d6f6dcde1128069a9935f8d25a93c9ac41e606a2
sunjae <zxsunje775@naver.com>
2023-07-14 18:22:04 +0900

    모든페이지 breeze이미지 삽입

--- src/page/MyPage.jsx
+++ src/page/MyPage.jsx
@@ -3,12 +3,27 @@ import BottomBar from "./component/SearchPageCom/bottomBar";
 import MyHeader from "./components/MyPage/MyHeader";
 import MyPageBody from "./components/MyPage/MyPageBody";
 import axios from "axios";
+import breeze from "./breeze.png";
+import styled from "styled-components";
+
 import UserTestImage from "./components/MyPage/UserTestImage.jpg";
 import { useParams } from "react-router-dom";
 
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.withCredentials = true;
+
+const BreezeImgBox = styled.div`
+  width: auto;
+  margin-top: 2rem;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const BreezeImg = styled.img`
+  width: 20rem;
+  height: auto;
+`;
 
 const MyPage = () => {
   const { Id3 } = useParams();
@@ -32,6 +47,10 @@ const MyPage = () => {
       <MyHeader UserInfo={UserInfo} UserImage={UserTestImage}></MyHeader>
       <MyPageBody UserInfo={UserInfo}></MyPageBody>
       <BottomBar IdOfUser={Id3}></BottomBar>
+
+      <BreezeImgBox>
+        <BreezeImg src={breeze}></BreezeImg>
+      </BreezeImgBox>
     </div>
   );
 };
