ec62ae0acd3d7b589f3591ddcfc72b69b4d9fcd4
sunjae <zxsunje775@naver.com>
2023-07-14 20:32:40 +0900

    search 투명화

--- src/page/component/SearchPageCom/searchBar.jsx
+++ src/page/component/SearchPageCom/searchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHome, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
@@ -7,19 +7,19 @@ import axios from "axios";
 library.add(faHome, faSearch, faUser);
 
 const SearchContainer = styled.div`
-  display : flex;
-  align-items : center;
+  display: flex;
+  align-items: center;
   width: 20rem;
   height: 4rem;
   position: relative;
   border: 0;
-  margin-left : 30px;
-  margin-top : 10px;
-`
+  margin-left: 30px;
+  margin-top: 10px;
+`;
 const SearchForm = styled.form`
-  width : 100%;
-  height : 60%;
-`
+  width: 100%;
+  height: 60%;
+`;
 const Search = styled.input`
   border: 0;
   padding-left: 10px;
@@ -30,11 +30,12 @@ const Search = styled.input`
   border-radius: 1rem;
   z-index: 3;
   position: absolute;
+  opacity: 0.9;
 `;
 
 const AutoSearchContainer = styled.div`
   z-index: 2;
-  height: 200px;
+  height: 20rem;
   width: 302px;
   background-color: #eaeaea;
   position: absolute;
@@ -42,11 +43,10 @@ const AutoSearchContainer = styled.div`
   /* border: 2px solid; */
   padding: 15px;
   border-radius: 1rem;
+  opacity: 0.9;
 `;
 
-const AutoSearchWrap = styled.ul`
-
-`;
+const AutoSearchWrap = styled.ul``;
 
 const AutoSearchData = styled.li`
   padding: 10px 8px;
@@ -68,52 +68,61 @@ const SearchBar = ({ onSearch }) => {
   const [suggestions, setSuggestions] = useState([]); // 자동완성 결과를 출력하기 위한 useState
   const [roomList, setRoomList] = useState([]); // 방 이름 목록을 저장하기 위한 useState
 
-  const handleInputChange = (e) => { // 검색어를 저장하는 함수
-    const {value} = e.target;
+  const handleInputChange = (e) => {
+    // 검색어를 저장하는 함수
+    const { value } = e.target;
     setQuery(value);
-    const suggestions  = getSuggestions(value);
+    const suggestions = getSuggestions(value);
     setSuggestions(suggestions);
   };
 
-  const handleSelectSuggestion = (suggestion) => { // 중간 검색 과정에서의 키워드를 저장하는 함수
+  const handleSelectSuggestion = (suggestion) => {
+    // 중간 검색 과정에서의 키워드를 저장하는 함수
     setQuery(suggestion);
     setSuggestions([]);
   };
 
-  const handleFormSubmit = (e) => { // 최종 검색 키워드를 저장하는 함수
+  const handleFormSubmit = (e) => {
+    // 최종 검색 키워드를 저장하는 함수
     e.preventDefault(); // 폼의 기본동작인 새로고침 방지
     const results = searchData(query); // 검색 결과 리턴
     setSearchResults(results); // 검색 결과 업데이트
-    if (typeof onSearch === "function") { // onSearch prop이 유효한 함수인지 확인
-        onSearch(results);
+    if (typeof onSearch === "function") {
+      // onSearch prop이 유효한 함수인지 확인
+      onSearch(results);
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     axios
-        .get(`${process.env.REACT_APP_API}/room_list_create/`)
-        .then((res)=>{
-            setRoomList(res.data);
-        })
-        .catch((e)=>{
-            console.log(e);
-        })
-},[suggestions]);
+      .get(`${process.env.REACT_APP_API}/room_list_create/`)
+      .then((res) => {
+        setRoomList(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [suggestions]);
 
   const getSuggestions = (value) => {
     // 검색어 자동 완성 결과를 가져오는 로직을 구현합니다.
     // 예시: 데이터는 배열 형태로 가정하고, 배열 요소 중에 입력된 검색어를 포함하는 항목들을 추출하여 반환합니다.
 
-    const filteredSuggestions = roomList.filter((item) => // filter로 true인 요소들만 results에 저장
-      item.name.includes(value) // item.name이 query의 내용을 포함하고 있는지 true/false로 리턴
+    const filteredSuggestions = roomList.filter(
+      (
+        item // filter로 true인 요소들만 results에 저장
+      ) => item.name.includes(value) // item.name이 query의 내용을 포함하고 있는지 true/false로 리턴
     );
     return filteredSuggestions;
   };
 
-  const searchData = (query) => { // query에 담긴 용어 기준으로 검색한 결과 리턴
+  const searchData = (query) => {
+    // query에 담긴 용어 기준으로 검색한 결과 리턴
 
-    const results = roomList.filter((item) => // filter로 true인 요소들만 results에 저장
-      item.name.includes(query) // item.name이 query의 내용을 포함하고 있는지 true/false로 리턴
+    const results = roomList.filter(
+      (
+        item // filter로 true인 요소들만 results에 저장
+      ) => item.name.includes(query) // item.name이 query의 내용을 포함하고 있는지 true/false로 리턴
     );
 
     return results;
@@ -123,32 +132,35 @@ const SearchBar = ({ onSearch }) => {
     <>
       <SearchContainer>
         <SearchForm onSubmit={handleFormSubmit}>
-          <Search style={{ paddingLeft: "10px" }} type="text" value={query} onChange={handleInputChange} placeholder="  Search..."></Search>
-          
+          <Search
+            style={{ paddingLeft: "10px" }}
+            type="text"
+            value={query}
+            onChange={handleInputChange}
+            placeholder="  Search..."
+          ></Search>
 
           {query.length > 0 && suggestions && (
             <AutoSearchContainer>
               <AutoSearchWrap>
                 {suggestions.map((suggestion) => (
-                  <AutoSearchData key={suggestion.id} 
-                  onClick={() => handleSelectSuggestion(suggestion)}
+                  <AutoSearchData
+                    key={suggestion.id}
+                    onClick={() => handleSelectSuggestion(suggestion)}
                   >
-                  {suggestion.name}
-                </AutoSearchData>
+                    {suggestion.name}
+                  </AutoSearchData>
                 ))}
               </AutoSearchWrap>
             </AutoSearchContainer>
           )}
 
-
           <ul>
             {/* 검색 결과창 */}
-          {searchResults.map((result) => (
-          <li key={result.id}>{result.name}</li>
-          ))}
-
+            {searchResults.map((result) => (
+              <li key={result.id}>{result.name}</li>
+            ))}
           </ul>
-
         </SearchForm>
       </SearchContainer>
     </>
